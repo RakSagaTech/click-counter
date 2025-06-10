d8114a3eaf243cfc3e3254dbbfb3fdd26071420e
rakesh <rakeshsagartfi21@gmail.com>
2025-06-10 10:16:00 +0000

    Added CSS & container for Button

--- src/components/ClickCounter/index.js
+++ src/components/ClickCounter/index.js
@@ -4,19 +4,26 @@ import {Component} from 'react'
 
 import './index.css'
 
-
-class ClickCounter extends Component{
-    render(){
-        return (
-            <div>
-                <h1> The Button has been clicked 
-                    <br/><span> 0 </span> times
-                </h1>
-                <p> Click the button to increase the count!</p>
-                <button type="button"> Click Me! </button>
-            </div>
-        )
-    }
+class ClickCounter extends Component {
+  render() {
+    return (
+      <div className="counter-container">
+        <h1 className="counter-heading">
+          {' '}
+          The Button has been clicked
+          <br />
+          <span className="value"> 0 </span> times
+        </h1>
+        <p className="description"> Click the button to increase the count!</p>
+        <div className="button-container">
+          <button type="button" className="button">
+            {' '}
+            Click Me!
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default ClickCounter
